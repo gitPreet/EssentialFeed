333485a1e04b063cb86af48b00890b9875adfdf2
Preetham Baliga <preethambaliga123@gmail.com>
2021-07-31 15:19:58 +0530

    Refactor test to remove duplication

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -25,34 +25,39 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    //we have started moving test logic to a seperate type. So production code is more generic.
-
-    var requestedURL: URL?  //HTTPClient will be production code, I do not want this requestedURL in production.
-    //It is only used for test purposes.
-
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_initDoesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(url: url, client: client)
+        let (_, client) = makeSUT()
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://another-url.com")!
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
 
         sut.load()
 
         XCTAssertEqual(client.requestedURL, url)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+
+    class HTTPClientSpy: HTTPClient {
+        //we have started moving test logic to a seperate type. So production code is more generic.
+
+        var requestedURL: URL?  //HTTPClient will be production code, I do not want this requestedURL in production.
+        //It is only used for test purposes.
+
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
